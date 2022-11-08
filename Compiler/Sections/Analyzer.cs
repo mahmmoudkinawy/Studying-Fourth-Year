787ffd6b32b38304374b8bcec1b7c997750dd0af
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-11-08 19:23:06 +0200

    Removed some code

--- Compiler/Sections/Analyzer.cs
+++ Compiler/Sections/Analyzer.cs
@@ -69,102 +69,15 @@ public class Analyzer
                 }
 
             }
-            else if (CheckOperator(item[i].ToString()))
+            else if (
+                item[i + 1].ToString().Equals(" ") ||
+                CheckDelimiter(item[i + 1].ToString()) ||
+                CheckOperator(item[i + 1].ToString()))
             {
-                if (i + 1 < item.Length && (CheckOperator(item.Substring(i, 2))))
-                    if (i + 2 < item.Length && CheckOperator(item.Substring(i, 3)))
-                    {
-                        token.Append(item.Substring(i, 3));
-                        item = item.Remove(i, 3);
-                        return Parse(token.ToString());
-                    }
-                    else
-                    {
-                        token.Append(item.Substring(i, 2));
-                        item = item.Remove(i, 2);
-                        return Parse(token.ToString());
-                    }
-                else if (CheckComments(item.Substring(i, 2)))
-                {
-                    if (item.Substring(i, 2).Equals("//"))
-                    {
-                        do
-                        {
-                            i++;
-                        } while (item[i] != '\n');
-                        item = item.Remove(0, i + 1);
-                        item = item.Trim(' ', '\t', '\r', '\n');
-                        i = -1;
-                    }
-                    else
-                    {
-                        do
-                        {
-                            i++;
-                        } while (item.Substring(i, 2).Equals("*/") == false);
-                        item = item.Remove(0, i + 2);
-                        item = item.Trim(' ', '\t', '\r', '\n');
-                        i = -1;
-                    }
-
-                }
-                else
-                {
-                    int ok;
-                    if (item[i] == '-' && Int32.TryParse(item[i + 1].ToString(), out ok))
-                        continue;
-                    token.Append(item[i]);
-                    item = item.Remove(i, 1);
-                    return Parse(token.ToString());
-                }
-
-            }
-            else
-                if (item[i] == '\'')
-            {
-                int j = i + 1;
-                if (item[j] == '\\')
-                    j += 2;
-                else
-                    j++;
-
-                token.Append("(literal constant, ").Append(item.Substring(i, j - i + 1)).Append(") ");
-                item = item.Remove(i, j - i + 1);
-                return token.ToString();
-            }
-            else
-                if (item[i] == '"')
-            {
-                int j = i + 1;
-                while (item[j] != '"')
-                    j++;
-                token.Append("(literal constant, ").Append(item.Substring(i, j - i + 1)).Append(") ");
-                item = item.Remove(i, j - i + 1);
-                return token.ToString();
-            }
-            else
-                if (item[i + 1].ToString().Equals(" ") || CheckDelimiter(item[i + 1].ToString()) == true || CheckOperator(item[i + 1].ToString()) == true)
-            {
-                if (Parse(item.Substring(0, i + 1)).Contains("numerical constant") && item[i + 1] == '.')
-                {
-                    int j = i + 2;
-                    while (item[j].ToString().Equals(" ") == false && CheckDelimiter(item[j].ToString()) == false && CheckOperator(item[j].ToString()) == false)
-                        j++;
-                    int ok;
-                    if (Int32.TryParse(item.Substring(i + 2, j - i - 2), out ok))
-                    {
-                        token.Append("(numerical constant, ").Append(item.Substring(0, j)).Append(") ");
-                        item = item.Remove(0, j);
-                        return token.ToString();
-                    }
-
-                }
                 token.Append(item.Substring(0, i + 1));
                 item = item.Remove(0, i + 1);
                 return Parse(token.ToString());
             }
-
-
         }
         return string.Empty;
     }
